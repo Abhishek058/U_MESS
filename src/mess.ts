a1891ad25bcf2fdc7a7cc55c4d2697b55f100afe
Abhishek <101443776+Abhishek058@users.noreply.github.com>
2024-09-03 07:25:02 +0530

    Update mess.ts
    added ' , ' insted of ' + '

--- src/mess.ts
+++ src/mess.ts
@@ -1,103 +1,103 @@
 export const menuB = [
   // Monday
   {
-    breakfast: "Vegetable Macroni , Bread Butter & Jam + Hot Milk",
-    lunch: "Rajmah Masala+ Aloo Jeera + Steameed Rice + Roti + Boondi Raita",
-    snack: "Samosa +Ketchup/Sonth Chutney + Tea",
-    dinner: "Moong Masoor Dal + Paneer Masala + steamed Rice+ Roti Gulab Jamun",
+    breakfast: "Vegetable Macroni , Bread Butter & Jam , Hot Milk",
+    lunch: "Rajmah Masala, Aloo Jeera , Steameed Rice , Roti , Boondi Raita",
+    snack: "Samosa ,Ketchup/Sonth Chutney , Tea",
+    dinner: "Moong Masoor Dal , Paneer Masala , steamed Rice, Roti Gulab Jamun",
   },
   // Tuesday
   {
-    breakfast: "Mix Aloo Parantha + Bread(Butter/Curd) + Pickle + Tea",
-    lunch: "Pindi Channa + Kadoo ki Sabzi/Aloo Capsicum + Steamed Rice + Roti + Mix Veg.Raita",
-    snack: "Cake + Tea",
-    dinner: "Dal Makhani + Bhind Masala + Steamed Rice + Roti + Pickle+ kulfi",
+    breakfast: "Mix Aloo Parantha , Bread(Butter/Curd) , Pickle , Tea",
+    lunch: "Pindi Channa , Kadoo ki Sabzi/Aloo Capsicum , Steamed Rice , Roti , Mix Veg.Raita",
+    snack: "Cake , Tea",
+    dinner: "Dal Makhani , Bhind Masala , Steamed Rice , Roti , Pickle, kulfi",
   },
   // wednesday
   {
-    breakfast: "Sweet Daliya+ Banana+ Masala Hot Sandwich + Coffee",
-    lunch: "Ghiya Channa Dal + Tamater Aloo Ki Sabji + Tadka Rice + Roti + Mint Ralta",
+    breakfast: "Sweet Daliya, Banana, Masala Hot Sandwich , Coffee",
+    lunch: "Ghiya Channa Dal , Tamater Aloo Ki Sabji , Tadka Rice , Roti , Mint Ralta",
     snack: "Patty, Tea",
-    dinner: "Saboot Masoor Dal + Veg Kormal/Mix Vegetable + Steamed Rice + Roti + Pickle + Cold Milk",
+    dinner: "Saboot Masoor Dal , Veg Kormal/Mix Vegetable , Steamed Rice , Roti , Pickle , Cold Milk",
   },
   // Thrusday
   {
     breakfast: "Mutter Kulcha Chutney. Poha /Plain Paratha Tea",
-    lunch: "Kadhi + Aloo Mutter+ Steamed Rice + Roti + Salad",
-    snack: "Poha Cutlet + Tea",
-    dinner: "Dal Tadka + Masala Gravy Chaap + Steamed Rice + Roti + Lados/Son Papdi/Balu Sahi",
+    lunch: "Kadhi , Aloo Mutter, Steamed Rice , Roti , Salad",
+    snack: "Poha Cutlet , Tea",
+    dinner: "Dal Tadka , Masala Gravy Chaap , Steamed Rice , Roti , Lados/Son Papdi/Balu Sahi",
   },
   // Friday
   {
-    breakfast: "Pav Bhaji Bread Toast+ Milk/(Plain Parantha sabzi) Tea",
-    lunch: "Black Channa + Chinese Nutri/ Aloo Nutri Masala + Steamed Rice + Roti + Boondi Raita",
-    snack: "Cold Sandwich + Nimbu Pani",
-    dinner: "Malka Dal + Mutter Paneer/Kadhal Paneer + Egg Curry + Steamed Rice + Roti + Rasgulla",
+    breakfast: "Pav Bhaji Bread Toast, Milk/(Plain Parantha sabzi) Tea",
+    lunch: "Black Channa , Chinese Nutri/ Aloo Nutri Masala , Steamed Rice , Roti , Boondi Raita",
+    snack: "Cold Sandwich , Nimbu Pani",
+    dinner: "Malka Dal , Mutter Paneer/Kadhal Paneer , Egg Curry , Steamed Rice , Roti , Rasgulla",
   },
   // Saturday
   {
     breakfast: "Namkeen Seviyan Bread Butter & Jam Banana Milk",
-    lunch: "Mix Dal/Red Lobhiya+ Malai Mushroom/Mushroom Do Pyaza+ Steamed Rice + Roti + Raita",
-    snack: "Namkeen/chips + Tea",
-    dinner: "Sambar + South Indian Aloo + Jeera rice + Roti +Gulab Jamun",
+    lunch: "Mix Dal/Red Lobhiya, Malai Mushroom/Mushroom Do Pyaza, Steamed Rice , Roti , Raita",
+    snack: "Namkeen/chips , Tea",
+    dinner: "Sambar , South Indian Aloo , Jeera rice , Roti ,Gulab Jamun",
   },
   // Sunday
   {
-    breakfast: "Poori + Aloo ki Sabzi+ Halwa Tea",
-    lunch: "Veg Noodles + Manchurian + Fried Rice",
-    snack: "Biscuit/Rusk + Coffee",
-    dinner: "Yellow Dal Gatta Masala Curry + Steamed Rice + Roti + Kheer",
+    breakfast: "Poori , Aloo ki Sabzi, Halwa Tea",
+    lunch: "Veg Noodles , Manchurian , Fried Rice",
+    snack: "Biscuit/Rusk , Coffee",
+    dinner: "Yellow Dal Gatta Masala Curry , Steamed Rice , Roti , Kheer",
   },
 ];
 
 export const menuG = [
    // Monday
    {
-    breakfast: "Vegetable Macroni , Bread Butter & Jam + Hot Milk",
-    lunch: "Rajmah Masala+ Aloo Jeera + Steameed Rice + Roti + Boondi Raita",
-    snack: "Cake + Tea",
-    dinner: "Moong Masoor Dal + Paneer Masala + steamed Rice+ Roti Gulab Jamun",
+    breakfast: "Vegetable Macroni , Bread Butter & Jam , Hot Milk",
+    lunch: "Rajmah Masala, Aloo Jeera , Steameed Rice , Roti , Boondi Raita",
+    snack: "Cake , Tea",
+    dinner: "Moong Masoor Dal , Paneer Masala , steamed Rice, Roti Gulab Jamun",
   },
   // Tuesday
   {
-    breakfast: "Mix Aloo Parantha + Bread(Butter/Curd) + Pickle + Tea",
-    lunch: "Pindi Channa + Kadoo ki Sabzi/Aloo Capsicum + Steamed Rice + Roti + Mix Veg.Raita",
-    snack: "Samosa + Tea",
-    dinner: "Dal Makhani + Bhind Masala + Steamed Rice + Roti + Pickle + kulfi",
+    breakfast: "Mix Aloo Parantha , Bread(Butter/Curd) , Pickle , Tea",
+    lunch: "Pindi Channa , Kadoo ki Sabzi/Aloo Capsicum , Steamed Rice , Roti , Mix Veg.Raita",
+    snack: "Samosa , Tea",
+    dinner: "Dal Makhani , Bhind Masala , Steamed Rice , Roti , Pickle , kulfi",
   },
   // wednesday
   {
-    breakfast: "Sweet Daliya + Banana+ Masala Hot Sandwich + Coffee",
-    lunch: "Ghiya Channa Dal + Tamater Aloo Ki Sabji + Tadka Rice + Roti + Mint Ralta",
+    breakfast: "Sweet Daliya , Banana, Masala Hot Sandwich , Coffee",
+    lunch: "Ghiya Channa Dal , Tamater Aloo Ki Sabji , Tadka Rice , Roti , Mint Ralta",
     snack: "Poha Cutlet, Tea",
-    dinner: "Saboot Masoor Dal + Veg Kormal/Mix Vegetable + Steamed Rice + Roti + Pickle + Cold Milk",
+    dinner: "Saboot Masoor Dal , Veg Kormal/Mix Vegetable , Steamed Rice , Roti , Pickle , Cold Milk",
   },
   // Thrusday
   {
-    breakfast: "Mutter Kulcha + Chutney + Poha /Plain Paratha Tea",
-    lunch: "Kadhi + Aloo Mutter+ Steamed Rice + Roti + Salad",
-    snack: "Pastry + Tea",
-    dinner: "Dal Tadka + Masala Gravy Chaap + Steamed Rice + Roti + Lados/Son Papdi/Balu Sahi",
+    breakfast: "Mutter Kulcha , Chutney , Poha /Plain Paratha Tea",
+    lunch: "Kadhi , Aloo Mutter, Steamed Rice , Roti , Salad",
+    snack: "Pastry , Tea",
+    dinner: "Dal Tadka , Masala Gravy Chaap , Steamed Rice , Roti , Lados/Son Papdi/Balu Sahi",
   },
   // Friday
   {
-    breakfast: "Pav Bhaji Bread Toast+ Milk/(Plain Parantha sabzi) Tea",
-    lunch: "Black Channa + Chinese Nutri/ Aloo Nutri Masala + Steamed Rice + Roti + Boondi Raita",
-    snack: "Cold Sandwich + Nimbu Pani",
-    dinner: "Malka Dal + Mutter Paneer/Kadhal Paneer + Egg Curry + Steamed Rice + Roti + Rasgulla",
+    breakfast: "Pav Bhaji Bread Toast, Milk/(Plain Parantha sabzi) Tea",
+    lunch: "Black Channa , Chinese Nutri/ Aloo Nutri Masala , Steamed Rice , Roti , Boondi Raita",
+    snack: "Cold Sandwich , Nimbu Pani",
+    dinner: "Malka Dal , Mutter Paneer/Kadhal Paneer , Egg Curry , Steamed Rice , Roti , Rasgulla",
   },
   // Saturday
   {
     breakfast: "Namkeen Seviyan Bread Butter & Jam Banana Milk",
-    lunch: "Mix Dal/Red Lobhiya+ Malai Mushroom/Mushroom Do Pyaza+ Steamed Rice + Roti + Raita",
-    snack: "Namkeen/chips + Tea",
-    dinner: "Sambar + South Indian Aloo + Jeera rice + Roti + Kheer",
+    lunch: "Mix Dal/Red Lobhiya, Malai Mushroom/Mushroom Do Pyaza, Steamed Rice , Roti , Raita",
+    snack: "Namkeen/chips , Tea",
+    dinner: "Sambar , South Indian Aloo , Jeera rice , Roti , Kheer",
   },
   // Sunday
   {
-    breakfast: "Poori + Aloo ki Sabzi+ Halwa Tea",
-    lunch: "Veg Noodles + Manchurian + Fried Rice",
-    snack: "Biscuit/Rusk + Coffee",
-    dinner: "Yellow Dal Gatta Masala Curry + Steamed Rice + Roti + Gulab Jamun",
+    breakfast: "Poori , Aloo ki Sabzi, Halwa Tea",
+    lunch: "Veg Noodles , Manchurian , Fried Rice",
+    snack: "Biscuit/Rusk , Coffee",
+    dinner: "Yellow Dal Gatta Masala Curry , Steamed Rice , Roti , Gulab Jamun",
   },
 ];
